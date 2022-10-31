cefe6f5a7971a43f9fb805b381be645025dbb263
Abh-ay <abhayyy1912@gmail.com>
2022-10-31 14:26:48 +0530

    change for django.settings.module

--- real_chat/asgi.py
+++ real_chat/asgi.py
@@ -8,13 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_chat.settings')
+import django
+django.setup()
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter,URLRouter
 import room.routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_chat.settings')
-import django
-django.setup()
 application = ProtocolTypeRouter(
     {
         "http":get_asgi_application(),
