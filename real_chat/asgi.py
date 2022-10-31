65d3a70e83f0b7b73efd80f223a27909c8c2e10a
Abh-ay <abhayyy1912@gmail.com>
2022-10-31 12:41:53 +0530

    edit config for procfile and asgi.py file

--- real_chat/asgi.py
+++ real_chat/asgi.py
@@ -8,13 +8,13 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter,URLRouter
 import room.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_chat.settings')
-
+django.setup()
 application = ProtocolTypeRouter(
     {
         "http":get_asgi_application(),
